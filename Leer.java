1fa8c65343fcc005de84ec65917005dfc6bbdbf1
alexvp21 <cf17alexander.vargas@iesjoandaustria.org>
2019-05-06 00:10:02 +0200

    Ejercicio 1 refactorizar

--- Leer.java
+++ Leer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 public class Leer {
    
@@ -11,28 +12,19 @@ public class Leer {
     public static void main(String[] args) {
         // Creamos la variables
        
-        File origen = new File("/home/alex/Documentos/Clase/M3/ficheros/alex_examen.txt");
+        File origen = new File("/home/alex/Documentos/Clase/M3/old/ficheros/alex_examen.txt");
         FileWriter destino = null;
         FileReader fr = null;
         PrintWriter escribir = null;
         BufferedReader leer = null;
        
     	try {
-    		destino = new FileWriter("/home/alex/Documentos/Clase/M3/ficheros/prova_AlexVargas.txt");
-            escribir = new PrintWriter(destino);
-            fr = new FileReader (origen);
-            leer = new BufferedReader(fr);
-           
-            String line;
-            while((line = leer.readLine())!=null) {
-            	escribir.println(line);            	
-            }
+    		escribir(origen, destino, fr, escribir, leer);
         }
         catch(Exception ex1){
             System.err.println("Error con algún fichero!");
         }finally {
             try{
-            	escribir.flush();
                 if((fr != null) && (destino != null)){
                     fr.close();
                     destino.close();
@@ -42,6 +34,21 @@ public class Leer {
             }
         }
        
+    }
+    
+    private static void escribir(File origen, FileWriter destino, FileReader fr, PrintWriter escribir, BufferedReader leer) throws IOException {
+    	destino = new FileWriter("/home/alex/Documentos/Clase/M3/old/ficheros/prova_AlexVargas.txt");
+        escribir = new PrintWriter(destino);
+        fr = new FileReader (origen);
+        leer = new BufferedReader(fr);
+       
+        String line;
+        while((line = leer.readLine())!=null) {
+        	escribir.println(line);            	
+        }
+        escribir.flush();
+        escribir.close();
+        leer.close();
     }
  
 }
